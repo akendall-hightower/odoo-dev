51d421271ca5ddf42398560bc2f160e1c0b7f80e
Alexander Kendall <akendall@hightowerproducts.com>
2024-10-25 20:09:19 +0000

    [ADD] View inheritance

--- motorcycle_stock/models/product_template.py
+++ motorcycle_stock/models/product_template.py
@@ -2,4 +2,6 @@ from odoo import models, fields
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
+
+    is_motorcycle = fields.Boolean(string="Motorcycle", default=False)
   
