89ba82a44d715d883e0e08519766ebe73aba3968
akendall-hightower <akendall@hightowerproducts.com>
2024-10-25 15:28:52 -0400

    Update __manifest__.py

---
+++ motorcycle_stock/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Motorcycle Stock",
+    "summary": "Motorcycle Stock Summary", 
+    "description": """
+    Motorcycle Stock Description
+    """,
+    "version": "0.1",
+    "category": "Kawiil/Registry",
+    "license": "OPL-1",
+    "depends": [], 
+    "data": [
+    ],
+    "demo": [
+    ],
+    "author": "Alexander Kendall",
+    "website": "hightowerproducts.com",
+    "application": True,
+    
+}
